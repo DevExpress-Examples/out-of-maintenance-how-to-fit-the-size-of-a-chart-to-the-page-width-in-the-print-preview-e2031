794cce4bd51a8897af13ceaddfa7da15e329a13b
DevExpressExampleBot <fakeEmail>
2023-05-18 17:35:37 +0400

    Source auto update [skip ci]

--- VB/PrintTheChart/Form1.vb
+++ VB/PrintTheChart/Form1.vb
@@ -1,34 +1,34 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraCharts.Printing
+
 ' ...
-
 Namespace PrintTheChart
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private cp As ChartPrinter
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Dim l As New Link(New PrintingSystem())
-			l.Landscape = True
-			l.PaperKind = System.Drawing.Printing.PaperKind.A3
-			cp = New ChartPrinter(Me.chartControl1)
-			cp.Initialize(l.PrintingSystem, l)
-			cp.SizeMode = PrintSizeMode.Stretch
-			AddHandler l.CreateDetailArea, AddressOf l_CreateDetailArea
-			l.ShowPreviewDialog()
-			cp.Release()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub l_CreateDetailArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
-			cp.CreateDetail(e.Graph)
-		End Sub
+        Private cp As ChartPrinter
 
-	End Class
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim l As Link = New Link(New PrintingSystem())
+            l.Landscape = True
+            l.PaperKind = System.Drawing.Printing.PaperKind.A3
+            cp = New ChartPrinter(chartControl1)
+            cp.Initialize(l.PrintingSystem, l)
+            cp.SizeMode = PrintSizeMode.Stretch
+            AddHandler l.CreateDetailArea, New CreateAreaEventHandler(AddressOf l_CreateDetailArea)
+            l.ShowPreviewDialog()
+            cp.Release()
+        End Sub
+
+        Private Sub l_CreateDetailArea(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+            cp.CreateDetail(e.Graph)
+        End Sub
+    End Class
 End Namespace
